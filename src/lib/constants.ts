d00159f9877334e7e00d542cd3a2054ed44b0aad
thisisfaisalhere <thisisfaisalhere@gmail.com>
2025-05-23 13:40:54 +0530

    chore: change restaurant name

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api'
 
-export const RESTAURANT_NAME = 'IKS Flavors'
+export const RESTAURANT_NAME = 'IKS'
 export const RESTAURANT_SUBTITLE = 'Indian Kitchen & Spices'
